6c41f8782f2fcb981331f9349a75753964fed054
akashchaurasiya1999 <167740312+akashchaurasiya1999@users.noreply.github.com>
2024-08-15 18:31:11 +0530

    newproject
    newproject

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import { RandomColor } from './components/random-color';
+
+function App() {
+  return (
+    <div className="App">
+      <RandomColor/>
+
+    </div>
+  );
+}
+
+export default App;
